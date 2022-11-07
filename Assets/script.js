065f751d306a9316544d3b54e7529625aa775989
HeyItsBradley <bradleyrodriguez77@gmail.com>
2022-11-06 17:49:22 -0700

    added variable to get the current hour

--- Assets/script.js
+++ Assets/script.js
@@ -1,6 +1,3 @@
-// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// the code isn't run until the browser has finished rendering all the elements
-// in the html.
 $(function () {
   // TODO: Add a listener for click events on the save button. This code should
   // use the id in the containing time-block as a key to save the user input in
@@ -14,10 +11,17 @@ $(function () {
   // attribute of each time-block be used to conditionally add or remove the
   // past, present, and future classes? How can Day.js be used to get the
   // current hour in 24-hour time?
+  var currentHour = dayjs().hour();
+  console.log(currentHour);
+
+  
   //
   // TODO: Add code to get any user input that was saved in localStorage and set
   // the values of the corresponding textarea elements. HINT: How can the id
   // attribute of each time-block be used to do this?
   //
   // TODO: Add code to display the current date in the header of the page.
+  var today = dayjs().format("dddd, MMMM D");
+  // this sets the current day to the days information on screen
+  $("#currentDay").text(today);
 });
